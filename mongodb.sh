9ad54658c22d24eea06439c8125b6faf6ab62888
PraveenEggadi <praveeneggadi2@gmail.com>
2025-05-10 18:01:42 +0530

    roboshop-shell script code

--- mongodb.sh
+++ mongodb.sh
@@ -11,13 +11,14 @@ TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
 echo "script started executing at $TIMESTAMP" &>> $LOGFILE
+
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo " $2 .... $R FAILED $N"
+        echo -e "ERROR: $2 has $R FAILED $N"
         exit 1
     else
-        echo "$2 .... $G SUCCESS! $N"
+        echo -e "$2 $G SUCCESS $N!"
     fi
 }
 
